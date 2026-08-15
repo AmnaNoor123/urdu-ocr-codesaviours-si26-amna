aea300c0d77f694ad4de8169ab8e104ee683879c
Amna Noor <shkhamna53@gmail.com>
2026-08-15 20:32:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ uploaded_file = st.file_uploader("Upload Urdu Image", type=["png", "jpg", "jpeg"
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     with st.spinner("Extracting text..."):
         result = reader.readtext(np.array(image), detail=0, paragraph=True)
         text = " ".join(result)
